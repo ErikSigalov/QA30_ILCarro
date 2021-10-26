9c60bceef564444a2040456791055d8514f0cc76
ErikSigalov <sigaloverik@gmail.com>
2021-10-26 10:36:42 +0300

    commit1

---
+++ src/main/java/manager/UserHelper.java
@@ -0,0 +1,16 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class UserHelper extends HelperBase{
+
+    public UserHelper(WebDriver wd) {
+        super(wd);
+    }
+
+    public void submitRegistration() {
+
+
+    }
+}
---
+++ src/test/java/Tests/LoginTests.java
@@ -0,0 +1,16 @@
+package Tests;
+
+
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+    @Test
+    public void test(){
+
+        String email = "eriknet2010@mail.ru";
+        String password = "Erik12345$";
+
+
+    }
+}
