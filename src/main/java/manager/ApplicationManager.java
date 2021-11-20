6876f64e155737c60c8228fdb2b4a47b52348352
ErikSigalov <sigaloverik@gmail.com>
2021-11-20 23:04:41 +0200

    new

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,6 +3,8 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,34 +17,44 @@ public class ApplicationManager {
     EventFiringWebDriver wd;
     UserHelper userHelper;
     CarHelper car;
+    String browser;
 
-
-
-
-    public void init(){
-        //wd = new ChromeDriver();
-        wd = new EventFiringWebDriver(new ChromeDriver());
-        logger.info("Tests starts on Chrome Driver");
-        wd.manage().window().maximize();
-        wd.navigate().to("https://ilcarro.xyz/search");
-        wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        userHelper =new UserHelper(wd);
-        car = new CarHelper(wd);
-        wd.register(new MyListener());
-
-    }
-    public void stop(){
-
-        logger.info("Tests passed");
-        wd.quit();
+    public ApplicationManager(String browser) {
+        this.browser = browser;
     }
 
-    public UserHelper getUserHelper() {
 
-        return userHelper;
-    }
-    public CarHelper getCar() {
+    public void init() {
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new EventFiringWebDriver(new ChromeDriver());
+            logger.info("Tests starts on Chrome Driver");
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new EventFiringWebDriver(new FirefoxDriver());
 
-        return car;
+            logger.info("Tests starts on FireFox Driver");
+
+            wd.manage().window().maximize();
+            wd.navigate().to("https://ilcarro.xyz/search");
+            wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            userHelper = new UserHelper(wd);
+            car = new CarHelper(wd);
+            wd.register(new MyListener());
+
+        }
     }
+        public void stop () {
+
+            logger.info("Tests passed");
+            wd.quit();
+        }
+
+        public UserHelper getUserHelper () {
+
+            return userHelper;
+        }
+        public CarHelper getCar () {
+
+            return car;
+        }
+
 }
--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -2,6 +2,7 @@ package test;
 
 import manager.ApplicationManager;
 import manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
@@ -12,7 +13,7 @@ import org.testng.annotations.BeforeSuite;
 import java.lang.reflect.Method;
 
 public class TestBase {
-    static ApplicationManager app =new ApplicationManager();
+    protected static ApplicationManager app =new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
